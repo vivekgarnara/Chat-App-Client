64c2a4a9548ccb0cbd9f93b1c6d96eba0d809c93
vivekgarnara <vivek.garnara@etatvasoft.com>
2024-07-31 18:29:41 +0530

    add MaterialUI in design and some changes

--- src/App.js
+++ src/App.js
@@ -1,53 +1,17 @@
 import './App.css';
-import io from 'socket.io-client';
-import { useEffect, useState } from 'react';
-
-const socket = io.connect("http://localhost:3001");
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Chat } from './Components/Chat/Chat';
+import { Login } from './Components/Login/Login';
 
 function App() {
-
-  const [messageToSend, setMessageToSend] = useState("");
-  const [storedMessages, setStoredMessages] = useState([]);
-
-  const sendMessage = () => {
-    if(messageToSend){
-      socket.emit("sendMessage", messageToSend );
-      setMessageToSend("");
-      setStoredMessages([...storedMessages, { type: "sent", content: messageToSend }]);
-    }
-  };
-
-  useEffect(() => {
-    socket.on("receivedMessage", (data) => {
-      console.log(data);
-      setStoredMessages((prevMessages) => [...prevMessages, { type: "received", content: data }]);
-    });
-
-    return () => {
-      socket.off("receivedMessage");
-    };
-  }, [socket]);
-
   return (
     <div className="App">
-      <div className='app-container'>
-        <div className='upr-section'>
-          <h2 className="section-title">Messages</h2>
-            <ul className="message-list">
-              {storedMessages.map((message, index) => (
-                <li key={index} className={`message-item ${message.type === "sent" ? "sent" : "received"}`}>
-                  <div className="message-content">{message.content}</div>
-                </li>
-              ))}
-            </ul>
-        </div>
-        <div>
-          <input className="message-input" placeholder="Enter message" value={messageToSend} onChange={(event) => {
-            setMessageToSend(event.target.value);
-          }} />
-          <button className="send-button" onClick={sendMessage}>Send</button>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/chat' element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Chat/Chat.js
@@ -0,0 +1,54 @@
+import './Chat.css';
+import io from 'socket.io-client';
+import { useEffect, useState } from 'react';
+import SendRoundedIcon from '@mui/icons-material/SendRounded';
+
+const socket = io.connect("http://localhost:3001");
+
+export function Chat() {
+
+  const [messageToSend, setMessageToSend] = useState("");
+  const [storedMessages, setStoredMessages] = useState([]);
+
+  const sendMessage = () => {
+    if (messageToSend) {
+      socket.emit("sendMessage", messageToSend);
+      setMessageToSend("");
+      setStoredMessages([...storedMessages, { type: "sent", content: messageToSend }]);
+    }
+  };
+
+  useEffect(() => {
+    socket.on("receivedMessage", (data) => {
+      console.log(data);
+      setStoredMessages((prevMessages) => [...prevMessages, { type: "received", content: data }]);
+    });
+
+    return () => {
+      socket.off("receivedMessage");
+    };
+  }, [socket]);
+
+  return (
+    <div className="abc">
+      <div className='app-container'>
+          <div className='upr-section'>
+            <h2 className="section-title">Messages</h2>
+            <ul className="message-list">
+              {storedMessages.map((message, index) => (
+                <li key={index} className={`message-item ${message.type === "sent" ? "sent" : "received"}`}>
+                  <div className="message-content">{message.content}</div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className='bottom-section'>
+            <input className="message-input" placeholder="Enter message" value={messageToSend} onChange={(event) => {
+              setMessageToSend(event.target.value);
+            }} />
+            <button className="send-button" onClick={sendMessage}><SendRoundedIcon/></button>
+          </div>
+        </div>
+    </div>
+  );
+}
